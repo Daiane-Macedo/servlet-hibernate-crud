024d221cc89b66586acb619d0c4f7aa9f3adc9d4
Daiane-Macedo <daianemacedo97@gmail.com>
2018-11-28 01:55:31 -0200

    implementação de view

---
+++ src/main/java/br/com/dac/servlet/MainServlet.java
@@ -0,0 +1,21 @@
+package br.com.dac.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "MainServlet",
+        urlPatterns = {"/"}
+)
+public class MainServlet extends HttpServlet{
+    @Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	        throws IOException, ServletException {
+	    req.getRequestDispatcher("/homeAdmin.jsp").forward(req, resp);
+	}
+}
---
+++ src/main/webapp/homeAdmin.jsp
@@ -0,0 +1,19 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html lang="en">
+<head>
+    <title>Home-Admin</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="/resources/home.css">
+   
+</head>
+<body>
+<div class="box">
+	<a href="/userList" id=user>Usuários</a>
+	<a href="/listaEstacoes">Estações</a>
+	<a href="/listaBikes">Bicicletas</a>	
+</div>
+</body>
+</html>
